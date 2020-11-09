752b846cdc34f9325687603b90a2c945a734971d
hugsi <hugo.almeida@hotmail.fr>
2020-11-09 12:23:36 +0100

    Ajout fichiers manquants

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ardoise;
+use App\Entity\Participant;
+use App\Form\ArdoiseSupprimerType;
+use App\Form\ParticipantSupprimerType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/participant", name="participant")
+     */
+    public function index()
+    {
+        return $this->render('participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+
+    /**
+     * @Route("/ardoises/supprimer/participants/{idPart}", name="participant_supprimer")
+     */
+    public function supprimer($idPart, Request $request)
+    {
+        //Initialiser le formulaire avec la bonne catégorie passée en parametre
+        $repo = $this->getDoctrine()->getRepository(Participant::class);
+        $participant= $repo->find($idPart);
+
+        //Création du fomulaire
+        $form = $this->createForm(ParticipantSupprimerType::class, $participant);
+
+        //récupération du POST
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //Récupération de l'entity manager
+            $em = $this->getDoctrine()->getManager();
+
+            // dire au manager qu'on veut garder notre objet dans la BDD
+            $em->remove($participant);
+
+            //générer l'update
+            $em->flush();
+
+            //aller à la liste des catégories
+            return $this->redirectToRoute("ardoises");
+        }
+
+        return $this->render("participant/supprimer.html.twig", [
+            "formulaire" => $form->createView(),
+            'participant' => $participant
+        ]);
+    }
+}
---
+++ src/Form/AjouterTransacType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Transaction;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AjouterTransacType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('valeur')
+            ->add('description')
+            ->add("ok", SubmitType::class, ["label"=>"Enregistrer",])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Transaction::class,
+        ]);
+    }
+}
